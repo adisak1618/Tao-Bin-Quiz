54bdc78836b9fc4ca360472909bd2eb8059070fe
Adisak Chaiyakul <adisakchaiyakul@gmail.com>
2024-02-01 18:46:28 +0700

    feat: add vending machine query

--- apps/web/graphql/generated/gql.ts
+++ apps/web/graphql/generated/gql.ts
@@ -13,6 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "query vendingMachine($vendingMachineId: String!) {\n  vendingMachine(id: $vendingMachineId) {\n    id\n    name\n    description\n    currentSales\n    lastBoots\n    logs {\n      title\n      description\n    }\n    status\n    statusMessage\n    config {\n      temperature\n      stockThresholds\n      operationalHours {\n        start\n        stop\n      }\n    }\n  }\n}": types.VendingMachineDocument,
     "query vendingMachines {\n  vendingMachines {\n    id\n    name\n    description\n    currentSales\n    lastBoots\n    logs {\n      title\n      description\n    }\n    status\n    statusMessage\n    config {\n      temperature\n      stockThresholds\n      operationalHours {\n        start\n        stop\n      }\n    }\n  }\n}": types.VendingMachinesDocument,
 };
 
@@ -30,6 +31,10 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query vendingMachine($vendingMachineId: String!) {\n  vendingMachine(id: $vendingMachineId) {\n    id\n    name\n    description\n    currentSales\n    lastBoots\n    logs {\n      title\n      description\n    }\n    status\n    statusMessage\n    config {\n      temperature\n      stockThresholds\n      operationalHours {\n        start\n        stop\n      }\n    }\n  }\n}"): (typeof documents)["query vendingMachine($vendingMachineId: String!) {\n  vendingMachine(id: $vendingMachineId) {\n    id\n    name\n    description\n    currentSales\n    lastBoots\n    logs {\n      title\n      description\n    }\n    status\n    statusMessage\n    config {\n      temperature\n      stockThresholds\n      operationalHours {\n        start\n        stop\n      }\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
